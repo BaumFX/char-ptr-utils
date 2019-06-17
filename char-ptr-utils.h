c9b988877aa992185a0278345b2754cbe04870b0
BaumFX <15325915+BaumFX@users.noreply.github.com>
2019-06-18 00:00:22 +0200

    added extensive comments

--- char-ptr-utils.h
+++ char-ptr-utils.h
@@ -8,10 +8,11 @@ namespace char_ptr_utils {
 	/**
 		function to get the index of a character in a string
 		@param string to search in, character to search for(, optional: offset)
-		@return position of character in string
+		@return position of character in string (-1 if char is not found)
 	*/
 	int index_of_char(char* array, char character, int offset = 0) 
 	{
+		//iterate over the string to find character, return current index if its found, -1 if its not found
 		for (int i = offset; i < strlen(array); i++)  { if (array[i] == character) { return i; } }
 		return -1;
 	}
@@ -19,28 +20,36 @@ namespace char_ptr_utils {
 	/**
 		function to get the index of a substring
 		@param string to search in, substring to find(, optional: offset)
-		@return position of substring in string
+		@return position of substring in string (-1 if substring is not found)
 	*/
 	int index_of_substr(char* array, char* substr, int offset = 0) {
+		//iterate over the string
 		for (int i = offset; i < strlen(array); i++)
 		{
+			//check if the current character matches the first character of the substring
 			if (array[i] == substr[0]) {
+				//iterate over the next (substring length) characters to determine whether they are the same
+				//in both strings, if they are, return the start index, if theyre not, return -1
 				for (int j = 0; i < strlen(substr); j++) { if (array[i + j] != substr[j]) { return -1; } }
 				return i;
 			}
 		}
+		//return -1 if the starting character of the substring is not found
 		return -1;
 	}
 
 	/**
 		function to get the last occurrence of a character in a string (function index_of_char reversed)
 		@param string to search in, character to search for(, optional: offset)
-		@return position of character in string
+		@return position of character in string (-1 if the character is not found)
 	*/
 	int last_index_of_char(char* array, char character, int offset = 0) {
+		//iterate over the string
 		for (int i = strlen(array) - 1; i > offset; i--) {
+			//if the string at the current index is the wanted character, return the index, proceed if its not
 			if (array[i] == character) { return i; }
 		}
+		//return -1 if the character is not found
 		return -1;
 	}
 
@@ -50,10 +59,15 @@ namespace char_ptr_utils {
 		@return substring of string
 	*/
 	char* substr(char* array, int offset, int length = -1) {
+		//get substring length
 		int len = strlen(array) - offset - (length == -1 ? 0 : strlen(array) - length);
+		//create new c-style string for our substring
 		char* dest = new char[len + 1];
+		//add terminating null sequence to our string (end of c-style string)
 		dest[len] = '\0';
+		//copy the part of the first string into the substring
 		memcpy(dest, &array[0] + offset, len);
+		//return the substring
 		return dest;
 	}
 
@@ -63,11 +77,16 @@ namespace char_ptr_utils {
 		@return combined string
 	*/
 	char* concat(char* array, char* array2) {
+		//calculate result string length
 		int len = strlen(array) + strlen(array2);
+		//create a new c-style string for our result
 		char* result = new char[len + 1];
+		//add terminating null sequence to our string (end of c-style string)
 		result[len] = '\0';
+		//copy the strings into our result
 		memcpy(result, &array[0], strlen(array));
 		memcpy(&result[strlen(array)], &array2[0], strlen(array2));
+		//return result
 		return result;
 	}
 
@@ -77,6 +96,7 @@ namespace char_ptr_utils {
 		@return bool if string starts with substring
 	*/
 	bool starts_with(char* array, char* wanted) {
+		//check if string starts with the wanted sequence
 		return strncmp(array, wanted, strlen(wanted)) == 0;
 	}
 
@@ -86,6 +106,7 @@ namespace char_ptr_utils {
 		@return string length
 	*/
 	int len(char* array) {
+		//return string length :moyai:
 		return strlen(array);
 	}
 }
